53ea8fae0deb8e2e0a0be3dd933af4d34611c6e7
wde4lee <adeee@foxmail.com>
2025-05-02 22:54:02 +0800

    20250502-更新

---
+++ web_h5/apps/storage/framework/views/07bd2118a113d69418e68a6d49897925ec29c1df.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\www\dev\2\fra-laravel-6-euj\laravel-6.18.35\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
